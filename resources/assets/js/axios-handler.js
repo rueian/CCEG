b8e5fd632ecb266b2ef50e49a2197ce57234b537
Rueian Oneecom <rueiancsie@gmail.com>
2017-12-15 00:09:14 +0800

    Fix js fetch error handling

--- resources/assets/js/axios-handler.js
+++ resources/assets/js/axios-handler.js
@@ -6,10 +6,15 @@ export function handleAxiosError(error) {
     if (error.response) {
         // The request was made and the server responded with a status code
         // that falls out of the range of 2xx
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-        alert('發送失敗: ' + error.response.data);
+        // console.log(error.response.data);
+        // console.log(error.response.status);
+        // console.log(error.response.headers);
+        if (error.response.data.message) {
+            alert('發送失敗: ' + error.response.data.message);
+        } else {
+            alert('發送失敗: ' + error.response.data);
+        }
+
         return;
     } else if (error.request) {
         // The request was made but no response was received
