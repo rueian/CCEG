861f9ad32bb733e77f726841ae8c7bd1ee2c7f4a
Rueian Oneecom <rueiancsie@gmail.com>
2017-12-16 03:46:00 +0800

    Column dropdown base on selected table

--- resources/assets/js/components/StepForm.js
+++ resources/assets/js/components/StepForm.js
@@ -3,12 +3,89 @@ import ReactDOM from 'react-dom';
 import Form from "react-jsonschema-form";
 import {refreshPage, handleAxiosError} from "../axios-handler";
 
+const ColumnSelector = (props) => {
+    let options = props.options;
+    let formData = props.formContext;
+
+    let schema = [];
+    if (options && options.inputKey) {
+        if (formData.inputs && formData.inputs[options.inputKey]) {
+            let storage = window.Props.blueprint.payload.storages[formData.inputs[options.inputKey]];
+            schema = storage.schema;
+        }
+    }
+
+    return (
+        <select className="form-control"
+               value={props.value}
+               required={props.required}
+               onChange={(event) => props.onChange(event.target.value)}>
+            <option key={'empty'} value={''} />
+            { schema.map(column => (
+                <option key={Math.random().toString(36).substring(2, 10)} value={column.name}>{column.name}</option>
+            )) }
+        </select>
+    );
+};
+
+const widgets = {
+    columnSelector: ColumnSelector
+};
+
+const uiSchema = {
+    param: {
+        target: {
+            "ui:widget": "columnSelector",
+            "ui:options": {
+                inputKey: "semi"
+            }
+        },
+        column: {
+            "ui:widget": "columnSelector",
+            "ui:options": {
+                inputKey: "input"
+            }
+        },
+        left: {
+            "ui:widget": "columnSelector",
+            "ui:options": {
+                inputKey: "left"
+            }
+        },
+        right: {
+            "ui:widget": "columnSelector",
+            "ui:options": {
+                inputKey: "right"
+            }
+        },
+        order: {
+            items: {
+                column: {
+                    "ui:widget": "columnSelector",
+                    "ui:options": {
+                        inputKey: "input"
+                    }
+                }
+            }
+        },
+        group: {
+            items: {
+                "ui:widget": "columnSelector",
+                "ui:options": {
+                    inputKey: "input"
+                }
+            }
+        },
+    }
+};
+
 export default class StepForm extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            type: ''
+            type: '',
+            formData: {}
         };
     }
 
@@ -24,16 +101,24 @@ export default class StepForm extends Component {
 
     handleTypeChange(e) {
         this.setState({
+            ...this.state,
             type: e.target.value
         });
     };
+
+    handleFormDataChange(e) {
+        this.setState({
+            ...this.state,
+            formData: e.formData
+        });
+    }
 
     render() {
         let form;
         let formSchema = this.props.stepFormSchema;
 
         if (this.state.type !== '') {
-            form = <Form schema={formSchema[this.state.type].schema} onSubmit={this.handleOnSubmit.bind(this)} />
+            form = <Form schema={formSchema[this.state.type].schema} formContext={this.state.formData} formData={this.state.formData} uiSchema={uiSchema} widgets={widgets} onChange={this.handleFormDataChange.bind(this)} onSubmit={this.handleOnSubmit.bind(this)} />
         }
 
         return (
